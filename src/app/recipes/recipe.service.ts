54843a95af5fd5900d9d96af716a0c787e4d9407
Sven Olding <sven.olding@gmx.de>
2024-01-14 10:00:08 +0100

    add data storage service to store using firebase realtime db

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -11,32 +11,32 @@ export class RecipeService {
   private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {
-    this.recipes = [
-      {
-        id: 1,
-        name: 'Schnitzel',
-        description: 'Ein leckeres Schnitzel',
-        imagePath:
-          'https://osnabrueck.wochenmarkt24.de/media/image/c6/87/6f/kloetzer_3912_600x600@2x.jpg',
-        ingredients: [
-          { name: 'Schnitzel', amount: 1 },
-          { name: 'Paniermehl', amount: 1 },
-          { name: 'Ei', amount: 1 },
-        ],
-      },
-      {
-        id: 2,
-        name: 'Chili sin carne',
-        description: 'Leckeres Chili ganz ohne Fleisch',
-        imagePath:
-          'https://img.ccnull.de/1030000/preview/1034728_a22b33c65f221a380e6550070a36e0ec.jpg',
-        ingredients: [
-          { name: 'Bohnen', amount: 1 },
-          { name: 'Mais', amount: 1 },
-          { name: 'Tomaten', amount: 1 },
-        ],
-      },
-    ];
+    // this.recipes = [
+    //   {
+    //     id: 1,
+    //     name: 'Schnitzel',
+    //     description: 'Ein leckeres Schnitzel',
+    //     imagePath:
+    //       'https://osnabrueck.wochenmarkt24.de/media/image/c6/87/6f/kloetzer_3912_600x600@2x.jpg',
+    //     ingredients: [
+    //       { name: 'Schnitzel', amount: 1 },
+    //       { name: 'Paniermehl', amount: 1 },
+    //       { name: 'Ei', amount: 1 },
+    //     ],
+    //   },
+    //   {
+    //     id: 2,
+    //     name: 'Chili sin carne',
+    //     description: 'Leckeres Chili ganz ohne Fleisch',
+    //     imagePath:
+    //       'https://img.ccnull.de/1030000/preview/1034728_a22b33c65f221a380e6550070a36e0ec.jpg',
+    //     ingredients: [
+    //       { name: 'Bohnen', amount: 1 },
+    //       { name: 'Mais', amount: 1 },
+    //       { name: 'Tomaten', amount: 1 },
+    //     ],
+    //   },
+    // ];
   }
 
   getRecipes() {
@@ -75,6 +75,11 @@ export class RecipeService {
         break;
       }
     }
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
     this.recipesChanged.next(this.recipes.slice());
   }
 }
---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, tap } from 'rxjs/operators';
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
+
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
+  private readonly FIREBASE_URL =
+    'https://recipe-book-86a36-default-rtdb.europe-west1.firebasedatabase.app/recipes.json';
+
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put(this.FIREBASE_URL, recipes).subscribe((response) => {
+      console.log(response);
+    });
+  }
+
+  fetchRecipes() {
+    return this.http.get<Recipe[]>(this.FIREBASE_URL).pipe(
+      map((recipes) => {
+        return recipes.map((recipe) => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : [],
+          };
+        });
+      }),
+      tap((recipes) => {
+        this.recipeService.setRecipes(recipes);
+      })
+    );
+  }
+}
